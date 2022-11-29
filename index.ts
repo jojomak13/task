100350b45001845ef5fda5cd3e4aa0d9848d65df
Joseph Makram <jojomak350@gmail.com>
2022-11-29 13:10:52 +0200

    Configure test setup file and database connection

--- index.ts
+++ index.ts
@@ -1,11 +1,24 @@
+import mongoose from 'mongoose';
 import app from './src/server';
 
 const start = async () => {
+    const envKeys = ['MONGO_URI'];
+
+    for (let key of envKeys) {
+        if (!process.env[key]) throw new Error(`[${key}] not found`);
+    }
+
+    try {
+        mongoose.connect(process.env.MONGO_URI!);
+    } catch (err) {
+        console.log(err);
+    }
+
     const port = process.env.PORT || 8080;
 
     app.listen(port, () => {
         console.log(`[Shipping Service] Running on port ${port}`);
     });
-}
+};
 
 start();
